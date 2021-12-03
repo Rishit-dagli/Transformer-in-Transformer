3e5b4081bb77e7203e6467dcb85736e5866aac37
Rishit Dagli <rishit.dagli@gmail.com>
2021-12-03 13:54:04 +0000

    create feedforward layer

--- tnt/tnt.py
+++ tnt/tnt.py
@@ -11,3 +11,22 @@ class PreNorm(tf.keras.layers.Layer):
     def call(self, x, **kwargs):
         x = self.norm(x)
         return self.fn(x, **kwargs)
+
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, dim, mult=4, dropout=0.0):
+        super(FeedForward, self).__init__()
+
+        self.net = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(dim * mult, input_dim=dim),
+                tf.keras.layers.Lambda(
+                    lambda x: x * tf.math.sqrt(0.5)
+                ),  # Gelu activation
+                tf.keras.layers.Dropout(dropout),
+                tf.keras.layers.Dense(dim, input_dim=dim * mult),
+            ]
+        )
+
+    def call(self, x):
+        return self.net(x)
